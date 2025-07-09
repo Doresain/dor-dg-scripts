93a82dbc2d15043205f1fd8001c50418734dba5e
James Standley <doresain@gmail.com>
2025-07-08 19:28:14 -0700

    Background color change

--- modules/lmje-background.mjs
+++ modules/lmje-background.mjs
@@ -1,6 +1,6 @@
 export function register() {
     Hooks.on("renderJournalEntryPageSheet", (app, html, data) => {
-        const backgroundStyle = "rgba(11, 10, 19, 0.9)";
+        const backgroundStyle = "#374e13";
         html.querySelectorAll("span.LMJE-link").forEach(span => {
             span.style.background = backgroundStyle;
         });
