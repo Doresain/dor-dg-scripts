20907de327f8329e3f32f7007f91931bc4bd76df
James Standley <doresain@gmail.com>
2025-07-08 19:16:19 -0700

    Background fix

--- modules/lmje-background.mjs
+++ modules/lmje-background.mjs
@@ -1,5 +1,5 @@
 export function register() {
-    Hooks.on("renderJournalPageSheet", (app, html, data) => {
+    Hooks.on("renderJournalEntryPageSheet", (app, html, data) => {
         const backgroundStyle = "rgba(11, 10, 19, 0.9)";
         html[0].querySelectorAll("span.LMJE-link").forEach(span => {
             span.style.background = backgroundStyle;
